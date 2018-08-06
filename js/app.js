495cefd8fff21f7bb006d82fee604fc454e3c492
Tatiana <tstanay8@gmail.com>
2018-08-06 12:41:06 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,6 +6,44 @@
             $(".search-bar").toggle();
         });
 
+
+        $("#range_50").ionRangeSlider({
+            type: "double",
+            min: 0,
+            max: 1000,
+            from: 200,
+            to: 500,
+            grid: true
+        });
+
+// Save slider instance to var
+        var slider = $("#range_50").data("ionRangeSlider");
+
+// Call sliders update method with any params
+        slider.update({
+            min: 100,
+            max: 500,
+            from: 150,
+            to: 450,
+            step: 50
+            // etc.
+        });
+
+        $("#range_51").ionRangeSlider({
+            type: "double",
+            min: 0,
+            max: 1000,
+            from: 200,
+            to: 500,
+            grid: true
+        });
+
+// Save slider instance to var
+        var slider = $("#range_51").data("ionRangeSlider");
+
+// Call sliders reset method
+        slider.reset();
+
 	});
 })(jQuery);
 
